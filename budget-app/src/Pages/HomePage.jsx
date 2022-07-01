a1ca2ef5a0c6863949d552526a95c9dd499d6482
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-01 01:27:15 -0400

    Formatted HomePage into a table format. Added titles for each column and a date column. Formatted the money values to look like usd.

--- budget-app/src/Pages/HomePage.jsx
+++ budget-app/src/Pages/HomePage.jsx
@@ -2,9 +2,25 @@ const HomePage = ({ transactions }) => {
   return (
     <div>
       <h1>Hompage</h1>
-      <div>
+      <div className="transactions-list">
+        <div className="transactions-list-title flex-row">
+          <div>Date</div>
+          <div>Paid</div>
+          <div>Saved</div>
+        </div>
         {transactions?.map((transaction) => {
-          return <div key={transaction.id}>{transaction.paid}</div>;
+          console.log(transaction.date.seconds * 1000);
+          const date = new Date(transaction.date.seconds * 1000);
+          return (
+            <div
+              className="flex-row transaction-list-item"
+              key={transaction.id}
+            >
+              <div>{date.toLocaleDateString()}</div>
+              <div>${(transaction.paid / 100).toFixed(2)}</div>{" "}
+              <div>${(transaction.saved / 100).toFixed(2)}</div>
+            </div>
+          );
         })}
       </div>
     </div>
