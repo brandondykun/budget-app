46399e886188a724c1bf7eecd0632b822d1172a9
Brandon Dykun <brandon.dykun@gmail.com>
2022-06-30 23:36:27 -0400

    Added firebase connection to App.js and fetching of transactions in useEffect.

--- budget-app/src/App.jsx
+++ budget-app/src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [transactions, setTransactions] = useState(null);
+
+  const transactionsRef = collection(db, "transactions");
+
+  useEffect(() => {
+    const getTransactions = async () => {
+      const transactions = await getDocs(transactionsRef);
+      const transactionsList = transactions.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log("transactions list: ", transactionsList);
+      setTransactions(transactionsList);
+    };
+    getTransactions();
+  }, []);
 
   return <div className="App"></div>;
 }
