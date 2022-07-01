5475ff96875e550a402d650805295613d6ab1391
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-01 13:40:29 -0400

    Added submit handler functionality to add the entry to firebase. Added a date input field. Added transactionsRef prop to component.

--- budget-app/src/Pages/AddTransactionPage.jsx
+++ budget-app/src/Pages/AddTransactionPage.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
+import { addDoc, Timestamp } from "firebase/firestore";
 
-const AddTransactionPage = () => {
+const AddTransactionPage = ({ transactionsRef }) => {
   const [savedAmount, setSavedAmount] = useState("");
   const [paidAmount, setPaidAmount] = useState("");
+  const [date, setDate] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    const timeMilliSeconds = Math.floor(new Date(date).getTime());
+    const fTimestamp = Timestamp.fromMillis(timeMilliSeconds);
+
+    const data = {
+      saved: savedAmount * 100,
+      paid: paidAmount * 100,
+      date: fTimestamp,
+    };
+
+    await addDoc(transactionsRef, data);
   };
 
   return (
     <div>
-      <h1>Add Transaction Page</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="paid">Paid</label>
-
+      <h1 className="page-title">Add Transaction</h1>
+      <form id="add-transaction-form" onSubmit={handleSubmit}>
+        <label htmlFor="paid">Amount Paid</label>
         <input
           type="number"
           min="0.00"
@@ -24,8 +36,8 @@ const AddTransactionPage = () => {
           value={paidAmount}
           onChange={(e) => setPaidAmount(e.target.value)}
         />
-        <label htmlFor="saved">Saved</label>
 
+        <label htmlFor="saved">Amount Saved</label>
         <input
           type="number"
           min="0.00"
@@ -37,7 +49,17 @@ const AddTransactionPage = () => {
           onChange={(e) => setSavedAmount(e.target.value)}
         />
 
-        <button type="submit">Add</button>
+        <label htmlFor="date">Date</label>
+        <input
+          type="datetime-local"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+
+        <button className="add-transaction-button" type="submit">
+          Add
+        </button>
       </form>
     </div>
   );
