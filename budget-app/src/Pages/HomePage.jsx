4a7ba89880adc852edb7da6b53564d2ef2baf4e6
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-01 13:37:03 -0400

    Converted to semantically correct table. Added state for totalPAid and totalSaved, and added useEffect to calculate those totals.

--- budget-app/src/Pages/HomePage.jsx
+++ budget-app/src/Pages/HomePage.jsx
@@ -1,29 +1,72 @@
+import { useEffect, useState } from "react";
+
 const HomePage = ({ transactions }) => {
+  const [totalPaid, setTotalPaid] = useState(0);
+  const [totalSaved, setTotalSaved] = useState(0);
+
+  useEffect(() => {
+    const paidTotal = transactions?.reduce((acc, curr) => {
+      return (acc += curr.paid);
+    }, 0);
+    const savedTotal = transactions?.reduce((acc, curr) => {
+      return (acc += curr.saved);
+    }, 0);
+
+    setTotalPaid(paidTotal);
+    setTotalSaved(savedTotal);
+  }, [transactions]);
+
   return (
-    <div>
-      <h1>Hompage</h1>
-      <div className="transactions-list">
-        <div className="transactions-list-title flex-row">
-          <div>Date</div>
-          <div>Paid</div>
-          <div>Saved</div>
-        </div>
-        {transactions?.map((transaction) => {
-          console.log(transaction.date.seconds * 1000);
-          const date = new Date(transaction.date.seconds * 1000);
-          return (
-            <div
-              className="flex-row transaction-list-item"
-              key={transaction.id}
-            >
-              <div>{date.toLocaleDateString()}</div>
-              <div>${(transaction.paid / 100).toFixed(2)}</div>{" "}
-              <div>${(transaction.saved / 100).toFixed(2)}</div>
-            </div>
-          );
-        })}
+    <>
+      <h1 className="page-title">Hompage</h1>
+      <div className="table-container">
+        <table id="transactions-table" className="transactions-list">
+          <tbody>
+            <tr className="transactions-list-title">
+              <th>Date</th>
+              <th>Paid</th>
+              <th>Saved</th>
+            </tr>
+            {transactions?.map((transaction) => {
+              const date = new Date(transaction.date.seconds * 1000);
+              return (
+                <tr className="transaction-list-item" key={transaction.id}>
+                  <td>{date.toLocaleDateString()}</td>
+                  <td>${(transaction.paid / 100).toFixed(2)}</td>
+                  <td>${(transaction.saved / 100).toFixed(2)}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <table id="totals-table">
+          <tbody>
+            <tr>
+              <td>Total</td>
+              <td>
+                $
+                {totalPaid &&
+                  (totalPaid / 100)
+                    .toFixed(2)
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </td>
+              <td>
+                $
+                {totalSaved &&
+                  (totalSaved / 100)
+                    .toFixed(2)
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </div>
+      <div>
+        You've saved{" "}
+        {totalSaved && totalPaid && parseInt((totalSaved / totalPaid) * 100)}%
+        of your earnings!
+      </div>
+    </>
   );
 };
 
