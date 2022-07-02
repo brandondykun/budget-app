4d2d0f12422d79c6ade69ecd1c8fb2156031fff6
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-02 11:14:11 -0400

    Added icons for login and signup. Added conditional rendering based on currentUser to display appropriate icons if user is logged in or not.

--- budget-app/src/Components/NavBar.jsx
+++ budget-app/src/Components/NavBar.jsx
@@ -5,30 +5,52 @@ import {
   faHouse,
   faCirclePlus,
   faRightFromBracket,
+  faRightToBracket,
+  faUserPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { auth } from "../firebase-config";
+import { AuthContext } from "../Auth";
+import { useContext } from "react";
 
 const NavBar = () => {
+  const { currentUser } = useContext(AuthContext);
+
   const logout = (e) => {
     signOut(auth)
       .then(() => {
         // Sign-out successful.
       })
       .catch((error) => {
-        // An error happened.
+        console.log(error);
       });
   };
   return (
     <nav className="primary-nav">
-      <NavLink className="nav-link" to="/">
-        <FontAwesomeIcon icon={faHouse} size="lg" />
-      </NavLink>
-      <NavLink className="nav-link" to="/add-transaction">
-        <FontAwesomeIcon icon={faCirclePlus} size="lg" />
-      </NavLink>
-      <div onClick={logout}>
-        <FontAwesomeIcon icon={faRightFromBracket} size="lg" />
-      </div>
+      {currentUser && (
+        <NavLink className="nav-link" to="/">
+          <FontAwesomeIcon icon={faHouse} size="lg" />
+        </NavLink>
+      )}
+      {currentUser && (
+        <NavLink className="nav-link" to="/add-transaction">
+          <FontAwesomeIcon icon={faCirclePlus} size="lg" />
+        </NavLink>
+      )}
+      {currentUser && (
+        <div onClick={logout}>
+          <FontAwesomeIcon icon={faRightFromBracket} size="lg" />
+        </div>
+      )}
+      {!currentUser && (
+        <NavLink className="nav-link" to="/login">
+          <FontAwesomeIcon icon={faRightToBracket} size="lg" />
+        </NavLink>
+      )}
+      {!currentUser && (
+        <NavLink className="nav-link" to="/sign-up">
+          <FontAwesomeIcon icon={faUserPlus} size="lg" />
+        </NavLink>
+      )}
     </nav>
   );
 };
