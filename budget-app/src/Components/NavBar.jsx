7713055c338a5e8570eac52c85ba36a2378f12b7
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-02 01:02:36 -0400

    Added font awesome icons and logout logic.

--- budget-app/src/Components/NavBar.jsx
+++ budget-app/src/Components/NavBar.jsx
@@ -1,14 +1,34 @@
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signOut } from "firebase/auth";
+import {
+  faHouse,
+  faCirclePlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { auth } from "../firebase-config";
 
 const NavBar = () => {
+  const logout = (e) => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
   return (
     <nav className="primary-nav">
       <NavLink className="nav-link" to="/">
-        Home
+        <FontAwesomeIcon icon={faHouse} size="lg" />
       </NavLink>
       <NavLink className="nav-link" to="/add-transaction">
-        Add Transaction
+        <FontAwesomeIcon icon={faCirclePlus} size="lg" />
       </NavLink>
+      <div onClick={logout}>
+        <FontAwesomeIcon icon={faRightFromBracket} size="lg" />
+      </div>
     </nav>
   );
 };
