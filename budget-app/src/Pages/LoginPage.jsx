a5caa8d825f4599a79155ade3075687c0dcaff28
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-02 00:59:01 -0400

    Added Login logic and form.

--- budget-app/src/Pages/LoginPage.jsx
+++ budget-app/src/Pages/LoginPage.jsx
@@ -1,7 +1,63 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+import { useState } from "react";
+import { auth } from "../firebase-config";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  // const auth = getAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setPersistence(auth, browserLocalPersistence).then(() => {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          if (user) {
+            navigate("/");
+          }
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setError(errorMessage);
+        });
+    });
+  };
+
   return (
     <div>
       <h1>Login Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Log In</button>
+      </form>
+      {error && <div>{error}</div>}
     </div>
   );
 };
