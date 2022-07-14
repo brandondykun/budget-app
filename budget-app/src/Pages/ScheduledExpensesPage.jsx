d16ba990796cc56f3438193e6e538d5bfaf92d3e
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-14 13:50:49 -0400

    Created ScheduledExpensesPage.

---
+++ budget-app/src/Pages/ScheduledExpensesPage.jsx
@@ -0,0 +1,67 @@
+// import { addDoc, Timestamp } from "firebase/firestore";
+import { db } from "../firebase-config";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { AuthContext } from "../Auth";
+import { useEffect, useState, useContext } from "react";
+import ExpenseList from "../Components/ExpenseList";
+import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const ScheduledExpensesPage = () => {
+  const [expenses, setExpenses] = useState([]);
+
+  const { currentUser } = useContext(AuthContext);
+  const expensesRef = collection(db, "expenses");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    let mounted = true;
+
+    const getExpenses = async () => {
+      try {
+        const expensesQuery = query(
+          expensesRef,
+          where("userId", "==", currentUser.uid)
+        );
+        const expenses = await getDocs(expensesQuery);
+        const expensesList = expenses.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        if (mounted) setExpenses(expensesList);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getExpenses();
+
+    return function cleanup() {
+      mounted = false;
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className="title-button-container">
+        <h1 className="page-title">scheduled expenses</h1>
+        <button
+          className="form-button add-expense-button"
+          onClick={() => navigate("/add-scheduled-expense")}
+        >
+          <FontAwesomeIcon icon={faPlus} size="sm" />
+        </button>
+      </div>
+      <div className="expense-lists-container">
+        <ExpenseList expenses={expenses} type={"want"} title={"wants"} />
+        <ExpenseList expenses={expenses} type={"need"} title={"needs"} />
+      </div>
+    </div>
+  );
+};
+
+export default ScheduledExpensesPage;
