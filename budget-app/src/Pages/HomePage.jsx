a2064b7336ba41dd79961e0c4961e8f01984b4fe
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-17 00:46:00 -0400

    Added div wrapper with class name page-contents-container.

--- budget-app/src/Pages/HomePage.jsx
+++ budget-app/src/Pages/HomePage.jsx
@@ -42,61 +42,63 @@ const HomePage = () => {
   });
 
   return (
-    <div className="homepage-container">
-      <h1 className="page-title">home</h1>
-      <div id="totals-table">
-        <div className="totals-table-total">
-          <div className="total-title">total paid: </div>
-          <div className="poppins-font">
-            $
-            {paidTotal &&
-              (paidTotal / 100)
-                .toFixed(2)
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+    <div className="page-contents-container">
+      <div className="homepage-container">
+        <h1 className="page-title">home</h1>
+        <div id="totals-table">
+          <div className="totals-table-total">
+            <div className="total-title">total paid: </div>
+            <div className="poppins-font">
+              $
+              {paidTotal &&
+                (paidTotal / 100)
+                  .toFixed(2)
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </div>
+          </div>
+          <div className="totals-table-total">
+            <div className="total-title">total saved:</div>
+            <div className="poppins-font">
+              $
+              {savedTotal &&
+                (savedTotal / 100)
+                  .toFixed(2)
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </div>
           </div>
         </div>
-        <div className="totals-table-total">
-          <div className="total-title">total saved:</div>
-          <div className="poppins-font">
-            $
-            {savedTotal &&
-              (savedTotal / 100)
-                .toFixed(2)
-                .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          </div>
+        <div className="totals-text">
+          you've saved{" "}
+          {savedTotal && paidTotal && parseInt((savedTotal / paidTotal) * 100)}%
+          of your earnings!
         </div>
-      </div>
-      <div className="totals-text">
-        you've saved{" "}
-        {savedTotal && paidTotal && parseInt((savedTotal / paidTotal) * 100)}%
-        of your earnings!
-      </div>
-      <div className="table-container">
-        <table id="transactions-table" className="transactions-list">
-          <tbody>
-            <tr className="transactions-list-title">
-              <th>date</th>
-              <th>paid</th>
-              <th>saved</th>
-            </tr>
-            {sortedTransactions?.map((transaction) => {
-              const date = new Date(transaction.date.seconds * 1000);
-              return (
-                <tr
-                  className="transaction-list-item poppins-font"
-                  key={transaction.id}
-                  onClick={() => navigate(`/details/${transaction.id}`)}
-                >
-                  <td>{date.toLocaleDateString()}</td>
-                  <td className="middle-column">
-                    ${(transaction.paid / 100).toFixed(2)}
-                  </td>
-                  <td>${(transaction.saved / 100).toFixed(2)}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+        <div className="table-container">
+          <table id="transactions-table" className="transactions-list">
+            <tbody>
+              <tr className="transactions-list-title">
+                <th>date</th>
+                <th>paid</th>
+                <th>saved</th>
+              </tr>
+              {sortedTransactions?.map((transaction) => {
+                const date = new Date(transaction.date.seconds * 1000);
+                return (
+                  <tr
+                    className="transaction-list-item poppins-font"
+                    key={transaction.id}
+                    onClick={() => navigate(`/details/${transaction.id}`)}
+                  >
+                    <td>{date.toLocaleDateString()}</td>
+                    <td className="middle-column">
+                      ${(transaction.paid / 100).toFixed(2)}
+                    </td>
+                    <td>${(transaction.saved / 100).toFixed(2)}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
