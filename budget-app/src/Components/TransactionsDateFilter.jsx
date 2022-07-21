39f6117e01a198c845a2712bff5564bcafa76553
Brandon Dykun <brandon.dykun@gmail.com>
2022-07-20 20:22:39 -0400

    Added select-container div around select elements.

--- budget-app/src/Components/TransactionsDateFilter.jsx
+++ budget-app/src/Components/TransactionsDateFilter.jsx
@@ -37,42 +37,46 @@ const TransactionsDateFilter = ({ transactions, setFilteredTransactions }) => {
       <div className="filter-inputs-container">
         <div className="filter-input-container">
           <label htmlFor="year">year</label>
-          <select
-            id="year"
-            name="year"
-            className="filter-select"
-            value={yearFilter}
-            onChange={(e) => setYearFilter(e.target.value)}
-          >
-            <option value={""}>all</option>
-            <option value={2021}>2021</option>
-            <option value={2022}>2022</option>
-            <option value={2023}>2023</option>
-          </select>
+          <div className="select-container">
+            <select
+              id="year"
+              name="year"
+              className="filter-select"
+              value={yearFilter}
+              onChange={(e) => setYearFilter(e.target.value)}
+            >
+              <option value={""}>all</option>
+              <option value={2021}>2021</option>
+              <option value={2022}>2022</option>
+              <option value={2023}>2023</option>
+            </select>
+          </div>
         </div>
         <div className="filter-input-container">
           <label htmlFor="month">month</label>
-          <select
-            id="month"
-            name="month"
-            className="filter-select"
-            value={monthFilter}
-            onChange={(e) => setMonthFilter(e.target.value)}
-          >
-            <option value={""}>all</option>
-            <option value={0}>january</option>
-            <option value={1}>february</option>
-            <option value={2}>march</option>
-            <option value={3}>april</option>
-            <option value={4}>may</option>
-            <option value={5}>june</option>
-            <option value={6}>july</option>
-            <option value={7}>august</option>
-            <option value={8}>september</option>
-            <option value={9}>october</option>
-            <option value={10}>november</option>
-            <option value={11}>december</option>
-          </select>
+          <div className="select-container">
+            <select
+              id="month"
+              name="month"
+              className="filter-select"
+              value={monthFilter}
+              onChange={(e) => setMonthFilter(e.target.value)}
+            >
+              <option value={""}>all</option>
+              <option value={0}>january</option>
+              <option value={1}>february</option>
+              <option value={2}>march</option>
+              <option value={3}>april</option>
+              <option value={4}>may</option>
+              <option value={5}>june</option>
+              <option value={6}>july</option>
+              <option value={7}>august</option>
+              <option value={8}>september</option>
+              <option value={9}>october</option>
+              <option value={10}>november</option>
+              <option value={11}>december</option>
+            </select>
+          </div>
         </div>
       </div>
     </div>
